ace6652722f7fc2245cada53782193d282576601
Zeynepp <zynpmico04@gmail.com>
2025-01-01 19:06:32 +0300

    Gömülü depo hatası düzeltildi

--- main.py
+++ main.py
@@ -20,16 +20,16 @@ print("4. Bölme")
 
 secim = input("Seçiminizi yapın (1/2/3/4): ")
 
-sayi3 = float(input("Birinci sayıyı girin lütfen: "))
-sayi4 = float(input("İkinci sayıyı girin lütfen: "))
+sayi1 = float(input("Birinci sayıyı girin: "))
+sayi2 = float(input("İkinci sayıyı girin: "))
 
 if secim == '1':
-    print("Sonuç:", toplama(sayi3, sayi4))
+    print("Sonuç:", toplama(sayi1, sayi2))
 elif secim == '2':
-    print("Sonuç:", cikarma(sayi3, sayi4))
+    print("Sonuç:", cikarma(sayi1, sayi2))
 elif secim == '3':
-    print("Sonuç:", carpma(sayi3, sayi4))
+    print("Sonuç:", carpma(sayi1, sayi2))
 elif secim == '4':
-    print("Sonuç:", bolme(sayi3, sayi4))
+    print("Sonuç:", bolme(sayi1, sayi2))
 else:
     print("Geçersiz seçim!")
